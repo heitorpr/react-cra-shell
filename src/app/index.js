aeb2d5183af934896334985cb847e3cf94e6cf7d
Heitor Polizeli Rodrigues <heitorprodrigues@gmail.com>
2021-10-10 17:28:59 -0300

    chore: add firebase analytics and hosting

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { Container, Box } from '@mui/material/';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 import Home from 'home';
 import ExFeature from 'exfeature';
 
@@ -8,6 +10,22 @@ const PAGES = {
   Home: '/',
   ExFeature: '/exfeature',
 };
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCTozrJtA52_LAxtdOU_jvDAvicG1B0j0A',
+  authDomain: 'react-cra-shell.firebaseapp.com',
+  projectId: 'react-cra-shell',
+  storageBucket: 'react-cra-shell.appspot.com',
+  messagingSenderId: '587613134512',
+  appId: '1:587613134512:web:cdac8b7580fdb485e7a575',
+  measurementId: 'G-0E2XJM1BJC',
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+getAnalytics(firebase);
 
 function App() {
   return (
